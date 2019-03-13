e093aab08993f58708d2ee7c87508a0ab7a46c88
Paul Bunyan <hal7df@gmail.com>
2019-03-13 16:49:36 -0400

    HttpJsonServlet: make factories static

--- src/main/java/coop/icc/escher/pinedesk/servlets/HttpJsonServlet.java
+++ src/main/java/coop/icc/escher/pinedesk/servlets/HttpJsonServlet.java
@@ -6,22 +6,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HttpJsonServlet extends HttpServlet {
-    HttpServlet () {
-        m_bldFactory = Json.createBuilderFactory(null);
-        m_wrFactory = Json.createWriterFactory(null)
+    static {
+        s_bldFactory = Json.createBuilderFactory(null);
+        s_wrFactory = Json.createWriterFactory(null)
     }
 
     protected void writeJson (HttpServletResponse resp, JsonObject obj,
                               boolean sendEmptyJson) throws ServletException {
         if (obj == null)
-            obj = m_bldFactory.createObjectBuilder().build();
+            obj = s_bldFactory.createObjectBuilder().build();
 
         try {
             if (response.getStatus() != HttpServletResponse.SC_NO_CONTENT
                 || (sendEmptyJson || obj.size() > 0)) {
                 setJsonResponse(response);
 
-                try (JsonWriter writer = m_wrFactory.createWriter(response.getWriter())) {
+                try (JsonWriter writer = s_wrFactory.createWriter(response.getWriter())) {
                     writer.writeObject(obj);
                 }
             }
@@ -35,14 +35,14 @@ public class HttpJsonServlet extends HttpServlet {
     protected void writeJson (HttpServletResponse resp, JsonArray arr,
                               boolean sendEmptyJson) throws ServletException {
         if (arr == null)
-            arr = m_bldFactory.createArrayBuilder().build();
+            arr = s_bldFactory.createArrayBuilder().build();
 
         try {
             if (response.getStatus() != HttpServletResponse.SC_NO_CONTENT
                 || (sendEmptyJson || arr.size() > 0)) {
                 setJsonResponse(response);
 
-                try (JsonWriter writer = m_wrFactory.createWriter(response.getWriter())) {
+                try (JsonWriter writer = s_wrFactory.createWriter(response.getWriter())) {
                     writer.writeArray(arr);
                 }
             }
@@ -64,17 +64,17 @@ public class HttpJsonServlet extends HttpServlet {
     }
 
     protected JsonObjectBuilder createObjectBuilder () {
-        return m_bldFactory.createObjectBuilder();
+        return s_bldFactory.createObjectBuilder();
     }
 
     protected JsonArrayBuilder createArrayBuilder () {
-        return m_bldFactory.createArrayBuilder();
+        return s_bldFactory.createArrayBuilder();
     }
 
     private static void setJsonResponse (HttpServletResponse response) {
         response.setHeader("Content-Type", "application/json; charset=utf-8");
     }
 
-    private JsonBuilderFactory m_bldFactory;
-    private JsonWriterFactory m_wrFactory;
+    private static JsonBuilderFactory s_bldFactory;
+    private static JsonWriterFactory s_wrFactory;
 }
